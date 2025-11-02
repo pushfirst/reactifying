709bb679a7a41ea0c4c8eaa4fe25cc94ff1fbb73
pushfirst <shakti.srivastava0509@gmail.com>
2025-11-02 20:03:56 +0530

    chore: expression in component

--- App.jsx
+++ App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const number = 1;
 const TitleComponent = () => (
   <h1 className="tile" tabIndex="1">
-    I am a title functional component
+    I am a title functional component {number}
   </h1>
 );
 // component composition
