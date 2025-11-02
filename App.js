36449910630e7639850fcaf545e6b19ff4bda100
pushfirst <shakti.srivastava0509@gmail.com>
2025-11-02 18:44:45 +0530

    chore: used vite and added scripts

--- App.js
+++ App.js
@@ -1,12 +1,5 @@
-// React.createElement("tag-name", attribute of tag like {id: 'heading'}, "content for the tag");
-const heading = React.createElement("h1", { id: 'heading', xyz: 1 }, "Hello World from React!");
-// Now react DOM will take this heading and place it in the root element
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(heading);
-
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const parent = React.createElement('div', { id: 'parent' },
     [
         React.createElement('div', { id: 'child' },
@@ -18,9 +11,11 @@ const parent = React.createElement('div', { id: 'parent' },
         React.createElement('div', { id: 'child2' },
             [
                 React.createElement('h1', {}, 'I am h1 tag div'),
-                React.createElement('h2', {}, 'I am h2 tag div')
+                React.createElement('h2', {}, 'I am h2 tag div'),
             ]
         )
     ]
 );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(parent);
