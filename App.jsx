884f22ff297d4a25753d9243c2c63f94c6ee817a
pushfirst <shakti.srivastava0509@gmail.com>
2025-11-02 20:03:00 +0530

    chore: component composition

--- App.jsx
+++ App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = (
-  <h1 className="head" tabIndex="1">
-    I am h1 tag jsx
+const TitleComponent = () => (
+  <h1 className="tile" tabIndex="1">
+    I am a title functional component
   </h1>
 );
+// component composition
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <TitleComponent />
+      <h1 className="head" tabIndex="1">
+        I am h1 heading functional component
+      </h1>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
