79e002ffca00371f0d38526dbc78fbdc7655e960
pushfirst <shakti.srivastava0509@gmail.com>
2025-11-02 19:28:18 +0530

    chore: bit about JSX

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const jsxHeading = (
+  <h1 className="head" tabIndex="1">
+    I am h1 tag jsx
+  </h1>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
